27ad1a9b4da79e19fe3fdefcf8daa61219545f8e
Sindhu-M29 <154402245+Sindhu-M29@users.noreply.github.com>
2024-01-31 15:17:41 +0530

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -5,3 +5,5 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Hello, this is a sample Python Web App running on Flask Framework!"
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8000)
